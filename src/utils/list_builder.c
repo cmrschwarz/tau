7f367493ee7068bd2fea2ebd6e6da1e43541fcde
Christian Schwarz <schwarz.christian.de@gmail.com>
2019-01-30 16:14:46 +0100

    allow for premem and postmem in list_builder

--- src/utils/list_builder.c
+++ src/utils/list_builder.c
@@ -41,27 +41,28 @@ int list_builder_add(list_builder* b, void* el){
     }
     return OK;
 }
-int list_builder_pop_list(
-    list_builder* b, void** list_start, void*** tgt, pool* tgtmem
+void** list_builder_pop_list(
+    list_builder* b, void** list_start, void*** tgt, pool* tgtmem,
+    ureg premem, ureg postmem
 ){
     ureg size = 0;
     list_build_segment* s = b->head_segment;
     if(s <= list_start && s->end > list_start){
         size = ptrdiff(b->head, list_start);
-        *tgt = pool_alloc(tgtmem, size);
-        if(!*tgt)return ERR;
-        memcpy(*tgt, list_start, size);
+        *tgt = pool_alloc(tgtmem, size + premem + postmem);
+        if(!*tgt)return NULL;
+        memcpy(ptradd(*tgt, premem), list_start, size);
         b->head = list_start;
-        return OK;
+        return ptradd(*tgt, size + premem + postmem);
     }
     size = ptrdiff(b->head, b->head_segment) - sizeof(list_build_segment);
     do{
         s = s->prev;
         size += ptrdiff(s->end, s) - sizeof(list_build_segment);       
     } while(list_start <= s || list_start >= s->end);
-    *tgt = (void**)pool_alloc(tgtmem, size);
-    if(!*tgt)return ERR;
-    void** h = *tgt;
+    *tgt = (void**)pool_alloc(tgtmem, size + premem + postmem);
+    if(!*tgt)return NULL;
+    void** h = ptradd(*tgt, premem);
     size = ptrdiff(s->end, list_start);
     memcpy(h, list_start, size);
     list_build_segment* head_old = b->head_segment;
@@ -74,4 +75,5 @@ int list_builder_pop_list(
         s = s->next;
     }
     b->head = list_start;
+    return ptradd(*tgt, size + premem + postmem);
 }
