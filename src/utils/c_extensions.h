9a25968c6c27797fe5e42a3cf51cf57ec930c191
Christian Schwarz <cmrs@cmrs.io>
2019-04-06 15:38:59 +0200

    remove broken case in PACK_ENUM macro

--- src/utils/c_extensions.h
+++ src/utils/c_extensions.h
@@ -4,11 +4,7 @@
 #if CMPLR_GCC || CMPLR_CLANG
 #define PACK_ENUM __attribute__((__packed__))
 #else
-#if __cplusplus
-#define PACK_ENUM : unsigned char //!!Fails for more than 256 elements
-#else
 #define PACK_ENUM
-#endif
 #endif
 
 // prevents emtpy transation unit warnings from ISO compliant compilers
