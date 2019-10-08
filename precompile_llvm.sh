2c6c132631803bc63e6833925f4443ed2f9dfc93
Christian Schwarz <cmrs@cmrs.io>
2019-10-08 22:02:42 +0200

    check for matching commit id in precompile_llvm.sh

--- precompile_llvm.sh
+++ precompile_llvm.sh
@@ -4,12 +4,19 @@ cd $SCRIPT_DIR
 
 #if this dir exists we already precompiled, so we exit successfully
 if [ -d "./deps/llvm-project-prebuild" ]; then
-    exit 0
+    cd ./deps/llvm-project/
+    #if we have the same commit id as during the prebuild exit successfully
+    if [ "$(git rev-parse HEAD)" == "$(cat ../llvm-project-prebuild/prebuild_commit_id.txt 2>/dev/null || : )" ]; then
+        echo "found existing prebuild"
+        exit 0
+    fi
+    cd ../../
+    rm -rf ./precomile_llvm
 fi
-
-# compile tauc with llvm as a non precompiled dependency in ./precompile_llvm
 mkdir precompile_llvm
 cd ./precompile_llvm
+
+# compile tauc with llvm as a non precompiled dependency in ./precompile_llvm
 cmake -DCMAKE_BUILD_TYPE=Release -DTAU_LLVM_PRECOMPILED:BOOL=OFF ../ || exit 1
 make -j$(nproc) || exit 1
 
@@ -18,9 +25,13 @@ mv ./deps/llvm-project ../deps/llvm-project-prebuild
 cd ..
 rm -rf ./precompile_llvm
 
+#store the llvm commit id
+cd ./deps/llvm-project
+git rev-parse HEAD > ../llvm-project-prebuild/prebuild_commit_id.txt
+
 # create empty files for the libs we don't need to stop llvm-config from complaining
 # we later check for these during linking and ignore them
-cd ./deps/llvm-project-prebuild/llvm/bin/
+cd ./../llvm-project-prebuild/llvm/bin/
 ./llvm-config --libs 2>&1 1>/dev/null | sed -n "s/.*error: missing: //p" | xargs touch
 
 # done
