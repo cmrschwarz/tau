7a82e6533f17776b77434653e8f870ed168492a2
Christian Schwarz <cmrs@cmrs.io>
2019-10-09 09:01:48 +0200

    run tests from home dir to make error paths complete

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -2,24 +2,28 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR/../"
 
-TAUC="../build/tauc"
+TAUC="./build/tauc"
 
 #run unit tests
 $TAUC -U
 # test other output modes with at least one run :) 
-$TAUC -L -S -E ./branchtest.t 
+$TAUC -L -S -E ./test/branchtest.t 
 ./a.out
 
-$TAUC ./bigtest/footest.t
+$TAUC ./test/bigtest/footest.t
 ./a.out
 
-$TAUC ./multimoduletest.t
+$TAUC ./test/multimoduletest.t
 ./a.out
 
-$TAUC ./structtest.t 
+$TAUC ./test/structtest.t 
 ./a.out
+
+rm ./a.out
+rm ./_.ll
+rm ./_.asm
 
 printf "\033[0;32mall tests passed\n"
 # - valgrind --track-origins=yes --leak-check=full --error-exitcode=1 --suppressions test/valgrind_suppressions$TAUC 
